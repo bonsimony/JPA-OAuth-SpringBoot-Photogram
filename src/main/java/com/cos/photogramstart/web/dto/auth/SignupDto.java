5ff86cbcba021909e0048c40f1278fbe6042dd16
UserK <UserK@Win11-01>
2024-08-06 22:08:02 +0900

    2024-08-06 회원가입 - 유효성 검사하기

--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,5 +1,8 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
@@ -7,10 +10,24 @@ import lombok.Data;
 //DTO(Data Transfer Obejct)
 @Data // Getter, Setter를 만들어 주는 어노테이션
 public class SignupDto {
-
+		
+	/*
+	 * pom.xml에 mavenRepository 사이트에서 Validation 태그 추가하여 
+	 * Validation 종류중에 @Max 어노테이션 활용
+	 */
+		@Max(20)
 		private String username;
+		
+		/* 
+		 * username과 마찬가지고 Validation 종류 중에 @NotBlack 사용 
+		 */
+		@NotBlank
 		private String password;
+		
+		@NotBlank
 		private String email;
+		
+		@NotBlank
 		private String name;
 		
 		public User toEntity() {
